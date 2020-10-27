1315c2b594d17f2d949d78ae1e8309e4b7e42192
Eudemar Tonatto <eudemar@hotmail.com>
2020-10-27 08:25:23 -0300

    Added Command Model to API Project

--- src/CommandAPI/Controllers/CommandsController.cs
+++ src/CommandAPI/Controllers/CommandsController.cs
@@ -2,7 +2,7 @@ using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
 namespace CommandAPI.Controllers
 {
-    [Route("app/[controller]")]
+    [Route("api/[controller]")]
     [ApiController]
     public class CommandsController : ControllerBase
     {
